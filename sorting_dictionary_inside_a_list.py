06c347fd24062d60db17cff43b7446b4a57e680f
hari2308 <35465681+hari2308@users.noreply.github.com>
2019-05-07 21:20:48 +0530

    Update sorting_dictionary_inside_a_list.py

--- sorting_dictionary_inside_a_list.py
+++ sorting_dictionary_inside_a_list.py
@@ -1,9 +1,14 @@
+#input list with dictionaries
 x = [{"b":1},{"d":2},{"a":3},{"c":4}]
+
+#list for sorting the keys 
 b = []
 for i in range(0,len(x)):
     for i in x[i]:
         b.append(i)
 b=sorted(b)
+
+#list for appending the sorted keys
 
 c =[]
 for y in b:
@@ -11,17 +16,21 @@ for y in b:
         if y in x[j].keys():
             c.append(x[j][y])
             
-e = []            
+#list for creating required output
+e = []
+#for tupling the sorted keys with values 
 d = zip(b,c)
-r = 0
-
+#iterating the tuples to add them in dictonary
 for x,y in d:
+    #dictonary for creating and appending in output list
     u = {}
+    #adding new key and value to dictnoary
     u[x] = y
+    #appending values to dictonary
     e.append(u)
+    # deleting dictonary in order to removed the added keys and values to output list
     del u
-    r +=1
-   
+#printing output list    
 print(e)
 
 #output:
